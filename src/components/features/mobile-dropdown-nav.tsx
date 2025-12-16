a267ea2a19e4bd13cfb6d6d5d023043d4017d8bb
Arek Chwedczuk <chwedczukarek@gmail.com>
2025-12-16 10:22:25 +0100

    fix: mobile version bugs etc

--- src/components/features/mobile-dropdown-nav.tsx
+++ src/components/features/mobile-dropdown-nav.tsx
@@ -15,12 +15,13 @@ export const MobileDropdownNav = () => {
   const [open, setOpen] = React.useState(false);
   return (
     <DropdownMenu onOpenChange={setOpen} open={open}>
-      <DropdownMenuTrigger asChild>
+      <DropdownMenuTrigger asChild className="translate-x-0 translate-y-0">
         <Button size="icon-lg" variant={open ? "default" : "ghost"}>
           {open ? <X /> : <Menu />}
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent
+        suppressHydrationWarning
         className="w-[90vw] h-[50vh]"
         side="bottom"
         align="end"
