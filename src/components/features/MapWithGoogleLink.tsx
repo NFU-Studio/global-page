e285a192d7bc13f041e9cfd928fbdea0bb08f9ce
Arek Chwedczuk <chwedczukarek@gmail.com>
2026-01-20 08:30:02 +0100

    feat: add new map no cookiez

---
+++ src/components/features/MapWithGoogleLink.tsx
@@ -0,0 +1,19 @@
+import {
+  Map,
+  MapMarker,
+  MarkerContent,
+  MarkerTooltip,
+} from "@/components/ui/map";
+
+export const MapWithGoogleLink = () => {
+  return (
+    <Map center={[17.035206299999995, 51.10823444723654]} zoom={15}>
+      <MapMarker latitude={51.10823444723654} longitude={17.035206299999995}>
+        <MarkerContent>
+          <div className="size-4 rounded-full border bg-accent"></div>
+        </MarkerContent>
+        <MarkerTooltip>Biuro</MarkerTooltip>
+      </MapMarker>
+    </Map>
+  );
+};
